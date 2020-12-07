42020262ec613adab4802444d84f1dff0ae3b27b
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-07 16:53:33 +0100

    method refactor to apply early return

--- day-1-report-repair/reportRepair.js
+++ day-1-report-repair/reportRepair.js
@@ -1,11 +1,11 @@
 const find = (entries, sumResultExpected) => {
-    if (entries.length != 0) {
-        const sumResult = entries[0] + entries[1];
-        if (sumResult === sumResultExpected) {
-            return entries;
-        }
-    }
-    return null;
+  if (entries.length == 0) return null;
+
+  const sumResult = entries[0] + entries[1];
+  if (sumResult === sumResultExpected) {
+    return entries;
+  }
+  return null
 }
 
 module.exports = { find };
