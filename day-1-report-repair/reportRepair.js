850469b641cd4d7acd7244726c75c8b974d9287c
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-07 16:40:54 +0100

    test to return null when doesn't have entries that matches with the sum result expected test refactor to included sumResultExpected

--- day-1-report-repair/reportRepair.js
+++ day-1-report-repair/reportRepair.js
@@ -1,6 +1,9 @@
-const find = (entries) => {
+const find = (entries, sumResultExpected) => {
     if (entries.length != 0) {
-        return entries;
+        const sumResult = entries[0] + entries[1];
+        if (sumResult === sumResultExpected) {
+            return entries;
+        }
     }
     return null;
 }
--- day-1-report-repair/test/reportRepairTest.js
+++ day-1-report-repair/test/reportRepairTest.js
@@ -5,18 +5,30 @@ describe('ReportRepair', function() {
   describe('find', function() {
     it('should return NULL given an empty array', function() {
         const entries = [];
+        const sumResultExpected = 0;
 
-        const result = report.find(entries);
+        const result = report.find(entries, sumResultExpected);
 
         assert.strictEqual(result, null);
     });
 
     it('should return the two numbers that sum results in 5', function() {
         const entries = [4, 1];
+        const sumResultExpected = 5;
         
-        const result = report.find(entries);
+        const result = report.find(entries, sumResultExpected);
 
         assert.deepStrictEqual(result, [4, 1]);
     });
+    
+    it('should return NULL given an inexistents numbers that match with the entries sum result 7', function(){
+        const entries = [3, 2];
+        const sumResultExpected = 7;
+
+        const result = report.find(entries, sumResultExpected);
+
+        assert.strictEqual(result, null);
+    });
+
   });
 });
