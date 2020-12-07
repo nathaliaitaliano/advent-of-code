a08939a50a72b6ed550625d28f48ed9641c2b6ed
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-07 20:40:52 +0100

    refactor for puzzle part 2 variables names refactor methods names refactor add console.log to return the final puzzle result part 2

--- day-1-report-repair/index.js
+++ day-1-report-repair/index.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
-const reportRepair = require('./reportRepair');
+const reportRepairPart1 = require('./reportRepairTwoEntries');
+const reportRepairPart2 = require('./reportRepairThreeEntries');
 
 const inputContent = fs.readFileSync('entries-input').toString().split('\n');
 
 const entries = inputContent.map(Number);
 
-console.log(reportRepair.multiply(reportRepair.find(entries, 2020)));
+console.log(reportRepairPart1.multiplyTwoEntries(reportRepairPart1.findTwoEntries(entries, 2020)));
+console.log(reportRepairPart2.multiplyThreeEntries(reportRepairPart2.findThreeEntries(entries, 2020)));
