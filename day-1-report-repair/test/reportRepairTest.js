665fd07e81c70ea01495ef668a1f20867e90e6e4
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-07 17:56:36 +0100

    test with input real data

--- day-1-report-repair/test/reportRepairTest.js
+++ day-1-report-repair/test/reportRepairTest.js
@@ -30,13 +30,22 @@ describe('ReportRepair', function() {
         assert.strictEqual(result, null);
     });
 
-    it('should return the two numbers, from a multiple entries, given a sum result in 6', function() {
+    it('should return the two numbers, from a multiple entries, given a sum result 6', function() {
         const entries = [3, 2, 4];
         const sumResultExpected = 6;
 
         const result = report.find(entries, sumResultExpected);
 
         assert.deepStrictEqual(result, [2, 4]);
+    });
+
+    it('should return the two numbers, from a multiple entries, given a sum result 2020', function() {
+        const entries = [1721, 979, 366, 299, 675, 1456];
+        const sumResultExpected = 2020;
+
+        const result = report.find(entries, sumResultExpected);
+
+        assert.deepStrictEqual(result, [1721, 299]);
     });
   });
 });
