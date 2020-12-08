ad3821c5ba245bf3f4f73cc46144f167126ec6d5
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-09 00:53:38 +0100

    test to invalidate the password without letter quantity expected

--- day-2-password-philosophy/test/passwordPhilosophyTest.js
+++ day-2-password-philosophy/test/passwordPhilosophyTest.js
@@ -1,32 +1,41 @@
-const assert = require('assert');
-const passwordPhilosophy = require('../passwordPhilosophy');
+const assert = require('assert')
+const passwordPhilosophy = require('../passwordPhilosophy')
 
 describe('PasswprdPhilosophy', function () {
     describe('findLetter', function () {
         it('should return FALSE given a password without the letter A', function () {
-            const letter = "a";
-            const password = "dog";
-            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter);
+            const letter = "a"
+            const password = "dog"
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter)
 
-            assert.strictEqual(resultAnalysisExpected, false);
-        });
+            assert.strictEqual(resultAnalysisExpected, false)
+        })
 
         it('should return TRUE given a password with the letter K', function () {
-            const letter = "k";
-            const password = "hacker";
-            const letterQuantityExpected = 1;
-            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected );
+            const letter = "k"
+            const password = "hacker"
+            const letterQuantityExpected = 1
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected )
 
-            assert.strictEqual(resultAnalysisExpected, true);
-        });
+            assert.strictEqual(resultAnalysisExpected, true)
+        })
 
         it('should return TRUE given a password have 2 letters F', function () {
-            const letter = "f";
-            const password = "falafel";
-            const letterQuantityExpected = 2;
-            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected);
+            const letter = "f"
+            const password = "falafel"
+            const letterQuantityExpected = 2
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected)
 
-            assert.strictEqual(resultAnalysisExpected, true);
-        });
-    });
-});
+            assert.strictEqual(resultAnalysisExpected, true)
+        })
+
+        it('should return FALSE given a password without 3 letters N', function () {
+            const letter = "n"
+            const password = "never"
+            const letterQuantityExpected = 3
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected)
+
+            assert.strictEqual(resultAnalysisExpected, false)
+        })
+    })
+})
