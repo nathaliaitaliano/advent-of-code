533d9c823155b4410ef7335452151bd4cfcd02f1
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-08 17:14:31 +0100

    test to return true given a password comparison with a existent letter

--- day-2-password-philosophy/test/passwordPhilosophyTest.js
+++ day-2-password-philosophy/test/passwordPhilosophyTest.js
@@ -5,10 +5,19 @@ describe('PasswprdPhilosophy', function () {
     describe('findLetter', function () {
         it('should return FALSE given a password without the letter A', function () {
             const letter = "a";
-            const password = "bcdefg";
+            const password = "dog";
             const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter);
 
             assert.strictEqual(resultAnalysisExpected, false);
         });
+
+        it('should return TRUE given a password with the letter K', function () {
+            const letter = "k";
+            const password = "hacker";
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter);
+
+            assert.strictEqual(resultAnalysisExpected, true);
+        });
+
     });
 });
