61547d92eea817dab8d53eb52c912f5cc82e1731
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-09 01:17:33 +0100

    test to validate a password that contains a letter with any of the quantities expected

--- day-2-password-philosophy/test/passwordPhilosophyTest.js
+++ day-2-password-philosophy/test/passwordPhilosophyTest.js
@@ -6,7 +6,8 @@ describe('PasswprdPhilosophy', function () {
         it('should return FALSE given a password without the letter A', function () {
             const letter = "a"
             const password = "dog"
-            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter)
+            const letterQuantityExpected = [1]
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected)
 
             assert.strictEqual(resultAnalysisExpected, false)
         })
@@ -14,7 +15,7 @@ describe('PasswprdPhilosophy', function () {
         it('should return TRUE given a password with the letter K', function () {
             const letter = "k"
             const password = "hacker"
-            const letterQuantityExpected = 1
+            const letterQuantityExpected = [1]
             const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected )
 
             assert.strictEqual(resultAnalysisExpected, true)
@@ -23,7 +24,7 @@ describe('PasswprdPhilosophy', function () {
         it('should return TRUE given a password have 2 letters F', function () {
             const letter = "f"
             const password = "falafel"
-            const letterQuantityExpected = 2
+            const letterQuantityExpected = [2]
             const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected)
 
             assert.strictEqual(resultAnalysisExpected, true)
@@ -32,10 +33,19 @@ describe('PasswprdPhilosophy', function () {
         it('should return FALSE given a password without 3 letters N', function () {
             const letter = "n"
             const password = "never"
-            const letterQuantityExpected = 3
+            const letterQuantityExpected = [3]
             const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected)
 
             assert.strictEqual(resultAnalysisExpected, false)
+        })
+
+        it('should return TRUE given a password that contains 1, 2, or 3 letters O', function () { 
+            const letter = "o"
+            const password = "good"
+            const letterQuantityExpected = [1, 2, 3]
+            const resultAnalysisExpected = passwordPhilosophy.analyse(password, letter, letterQuantityExpected)
+
+            assert.strictEqual(resultAnalysisExpected, true)
         })
     })
 })
