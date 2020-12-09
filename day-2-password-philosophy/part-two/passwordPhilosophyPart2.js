88d238a1974702289d7a0c5c662385f8aec11724
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-09 23:12:27 +0100

    refactor to apply DRY and KISS

--- day-2-password-philosophy/part-two/passwordPhilosophyPart2.js
+++ day-2-password-philosophy/part-two/passwordPhilosophyPart2.js
@@ -1,5 +1,4 @@
-const analyse = (firstPositionExpected, secondPositionExpected, letter, password) => {
-    return ((password.charAt(firstPositionExpected - 1) === letter) || (password.charAt(secondPositionExpected - 1) === letter)) && (password.charAt(firstPositionExpected - 1) != (password.charAt(secondPositionExpected - 1)))
-}
+const analyse = (firstPositionExpected, secondPositionExpected, letter, password) => (password.charAt(firstPositionExpected - 1) === letter) != (password.charAt(secondPositionExpected - 1) === letter)
+
 
 module.exports = { analyse }
