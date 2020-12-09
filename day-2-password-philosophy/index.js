44f3b7c8d48d80f51582a96dcd6650b54160e1d7
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-09 19:02:14 +0100

    implementation for the result of puzzle - part 1 creation of index.js validation of passwords input

--- day-2-password-philosophy/index.js
+++ day-2-password-philosophy/index.js
@@ -1,6 +1,19 @@
 const fs = require('fs')
 const passwordPhilosohy = require('./passwordPhilosophy')
+const inputParser = require('./inputParser')
 
-const passwordsDataContent = fs.readFileSync('input-passwords').toString().split('\n')
+const inputDataContent = fs.readFileSync('input-passwords').toString().split('\n')
+let validPasswords = 0
 
-console.log(passwordsDataContent)
+for (i = 0; i < inputDataContent.length; i++) {
+  let passwordData
+  let passwordDataAnalysis
+
+  passwordData = inputParser.parse(inputDataContent[i])
+  passwordDataAnalysis = passwordPhilosohy.analyse(passwordData.letterMinQuantityExpected, passwordData.letterMaxQuantityExpected, passwordData.letter, passwordData.password)
+
+  if (passwordDataAnalysis === true) {
+    validPasswords += 1
+  }
+}
+console.log(validPasswords)
