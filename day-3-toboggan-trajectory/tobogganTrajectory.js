d2c9e8793a622ca279adda629d5dae6d0b0b1f46
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-10 17:54:44 +0100

    variable name refactor to attend better the puzzle specifications

--- day-3-toboggan-trajectory/tobogganTrajectory.js
+++ day-3-toboggan-trajectory/tobogganTrajectory.js
@@ -1,10 +1,10 @@
 const countTrees = (map) => {
   let treesQuantity = 0
-  let column = 0
+  let slopeCoordinate = 0
 
   for (let i = 1; i < map.length; i++) {
-    column = column + 3
-    let toboggan = map[i].charAt(column)
+    slopeCoordinate = slopeCoordinate + 3
+    let toboggan = map[i].charAt(slopeCoordinate)
 
     if (toboggan === "#") {
       treesQuantity += 1
