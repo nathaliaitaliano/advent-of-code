c1ae58fb782b0b27b80f32d6f9c22848d3b3eee2
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-10 22:50:13 +0100

    test to search for trees given a map coordinate to slope

--- day-3-toboggan-trajectory/test/tobogganTrajectoryTest.js
+++ day-3-toboggan-trajectory/test/tobogganTrajectoryTest.js
@@ -5,34 +5,42 @@ describe('TobogganTrajectory', function () {
   describe('countTrees', function () {
     it('should return 0 tree', function () {
       const map = [".........", "........."]
-      const treesExpected = 0
+      const treesQuantityExpected = 0
       const treesQuantity = tobogganTrajectory.countTrees(map)
 
-      assert.strictEqual(treesExpected, treesQuantity)
+      assert.strictEqual(treesQuantityExpected, treesQuantity)
     })
 
     it('should return 1 tree', function () {
-      const map = ["...##.##.", "..###...."]
-      const treesExpected = 1
+      const map = ["...##.##..", "..###...#."]
+      const treesQuantityExpected = 1
       const treesQuantity = tobogganTrajectory.countTrees(map)
 
-      assert.strictEqual(treesExpected, treesQuantity)
+      assert.strictEqual(treesQuantityExpected, treesQuantity)
     })
 
     it('should return 2 trees', function () {
-      const map = [".....####", "..######", ".....###"]
-      const treesExpected = 2
+      const map = [".....#####", "..######.", ".....###."]
+      const treesQuantityExpected = 2
       const treesQuantity = tobogganTrajectory.countTrees(map)
 
-      assert.strictEqual(treesExpected, treesQuantity)
+      assert.strictEqual(treesQuantityExpected, treesQuantity)
     })
 
     it('should return 3 trees', function () {
       const map = ["##########", "##########", "##########", "###.......", "#######..."]
-      const treesExpected = 3
+      const treesQuantityExpected = 3
       const treesQuantity = tobogganTrajectory.countTrees(map)
 
-      assert.strictEqual(treesExpected, treesQuantity)
+      assert.strictEqual(treesQuantityExpected, treesQuantity)
+    })
+
+    it('should return 4 trees on the map given a coordinate pattern (x=3, y=1', function () {
+      const map = ["##########", "######....", ".#.#.#####", "###...####", ".#.######"]
+      const treesQuantityExpected = 4
+      const xCoordinate = 3
+      const yCoordinate = 1
+      const treesQuantity = tobogganTrajectory.countTrees(map, xCoordinate, yCoordinate)
     })
   })
 })
