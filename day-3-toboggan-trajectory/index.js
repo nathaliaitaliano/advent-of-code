43ff7ebe49e8335d303f011ca26c1ba6515e7adc
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-10 22:52:13 +0100

    refactor to get the aswer for the second part of puzzle

--- day-3-toboggan-trajectory/index.js
+++ day-3-toboggan-trajectory/index.js
@@ -2,7 +2,16 @@ const fs = require('fs')
 const tobogganTrajectory = require('./tobogganTrajectory')
 
 const map = fs.readFileSync('map-input').toString().split('\n')
+const coordinates = [
+  { xCoordinate: 1, yCoordinate: 1 },
+  { xCoordinate: 3, yCoordinate: 1 },
+  { xCoordinate: 5, yCoordinate: 1 },
+  { xCoordinate: 7, yCoordinate: 1 },
+  { xCoordinate: 1, yCoordinate: 2 }]
+let treesQuantity = 1
 
-const treesQuantity = tobogganTrajectory.countTrees(map)
+for (let i = 0; i < coordinates.length; i++) {
+  treesQuantity *= (tobogganTrajectory.countTrees(map, coordinates[i].xCoordinate, coordinates[i].yCoordinate))
+}
 
 console.log(treesQuantity)
