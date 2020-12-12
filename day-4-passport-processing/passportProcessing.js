66c0c00465b9c20ec1b68c8159cf19e0c96fe567
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-12 10:31:02 +0100

    method refactor to invalidate passport without expiration year

--- day-4-passport-processing/passportProcessing.js
+++ day-4-passport-processing/passportProcessing.js
@@ -1,5 +1,5 @@
 const validate = (passportData) => {
-  return ((passportData.byr) && (passportData.iyr) && (passportData.pid))
+  return ((passportData.byr) && (passportData.iyr) && (passportData.eyr) &&(passportData.pid))
 }
 
 module.exports = { validate }
