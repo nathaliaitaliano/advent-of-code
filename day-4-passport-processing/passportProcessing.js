684c4afeb85de6f89a443343e1a320f7035251f8
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-12 10:42:46 +0100

    method refactor to invalidate a passport without eye color data

--- day-4-passport-processing/passportProcessing.js
+++ day-4-passport-processing/passportProcessing.js
@@ -1,5 +1,5 @@
 const validate = (passportData) => {
-  return ((passportData.byr) && (passportData.iyr) && (passportData.eyr) && (passportData.hgt) && (passportData.hcl))
+  return ((passportData.byr) && (passportData.iyr) && (passportData.eyr) && (passportData.hgt) && (passportData.hcl) && (passportData.ecl))
 }
 
 module.exports = { validate }
