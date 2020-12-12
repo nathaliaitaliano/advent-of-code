5d12cedb767e6aa0c0903ce2e0e9424ff51b0bc1
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-12 10:14:04 +0100

    test to invalidate a passport without issue year

--- day-4-passport-processing/test/passportProcessingTest.js
+++ day-4-passport-processing/test/passportProcessingTest.js
@@ -4,13 +4,21 @@ const passportProcessing = require('../passportProcessing')
 describe('PassportProcessing', function () {
   describe('validatePassport', function () {
     it('should return FALSE given a password without the birth year', function () {
-      const passportData = { pid: 860033327, eyr: 2022 }
+      const passportData = {ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", cid: 147, hgt: "183cm"}
       const passportValidation = passportProcessing.validatePassport(passportData)
 
       assert.ok(!passportValidation)
     })
 
-    it('should return TRUE given a passport that contains the birth year', function () {
+    it('should return FALSE given a passport without issue year', function () {
+      const passportData = {ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, cid: 147, hgt: "183cm"}
+      const passportValidation = passportProcessing.validatePassport(passportData)
+
+      assert.ok(!passportValidation)
+    
+    })
+
+    xit('should return TRUE given a passport that contains the birth year,', function () {
       const passportData = { byr: 1989, pid: 118755096 }
       const passportValidation = passportProcessing.validatePassport(passportData)
 
