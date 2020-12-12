9edc883e517bd60f9984b1102bd9bee0521259b2
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-12 11:04:12 +0100

    test for passport that contains country ID data

--- day-4-passport-processing/test/passportProcessingTest.js
+++ day-4-passport-processing/test/passportProcessingTest.js
@@ -4,56 +4,63 @@ const passportProcessing = require('../passportProcessing')
 describe('PassportProcessing', function () {
   describe('validate', function () {
     it('should return FALSE given a password without birth year data', function () {
-      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", cid: 147, hgt: "183cm" }
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FALSE given a passport without issue year data', function () {
-      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, cid: 147, hgt: "183cm" }
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FLASE given a passport without expiration year data', function () {
-      const passportData = {ecl: "gry", pid: 860033327, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147, hgt: "183cm"}
+      const passportData = { ecl: "gry", pid: 860033327, hcl: "#fffffd", byr: 1937, iyr: 2017, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FALSE given a passport without height data', function () {
-      const passportData = {ecl: "gry", eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147 }
+      const passportData = { ecl: "gry", eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147 }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FALSE given a passport without hair color data', function () {
-      const passportData = {ecl: "gry", pid: 860033327, eyr: 2020, byr: 1937, iyr: 2017, cid: 147, hgt: "183cm"}
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, byr: 1937, iyr: 2017, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FALSE given a passport without eye color data', function () {
-      const passportData = { pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147, hgt: "183cm"}
+      const passportData = { pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FALSE given a passport without passport ID', function () {
-      const passportData = {ecl: "gry", eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147, hgt: "183cm"}
+      const passportData = { ecl: "gry", eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return TRUE given a passport that contains the birth year, issue year, expiration year, height, hair color, eye color and passport ID data', function () {
-      const passportData = {ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147, hgt: "183cm"}
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, hgt: "183cm" }
+      const passportValidation = passportProcessing.validate(passportData)
+
+      assert.ok(passportValidation)
+    })
+
+    it('should return TRUE given a passport that contains country ID data besides all the others data informations', function () {
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, iyr: 2017, cid: 147, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(passportValidation)
