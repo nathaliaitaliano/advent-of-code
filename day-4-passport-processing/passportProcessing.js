27726e1d6e67b27c0f1e75432d61cd25197513f2
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-12 10:53:06 +0100

    method refactor to invalidate a passport without passport ID data

--- day-4-passport-processing/passportProcessing.js
+++ day-4-passport-processing/passportProcessing.js
@@ -1,5 +1,5 @@
 const validate = (passportData) => {
-  return ((passportData.byr) && (passportData.iyr) && (passportData.eyr) && (passportData.hgt) && (passportData.hcl) && (passportData.ecl))
+  return ((passportData.byr) && (passportData.iyr) && (passportData.eyr) && (passportData.hgt) && (passportData.hcl) && (passportData.ecl) && (passportData.pid))
 }
 
 module.exports = { validate }
