2761eac85e48a8be7eb4e3c3270479073e0d48c0
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-12 10:17:58 +0100

    test to invalidate a passport that not contains the birth and issue year

--- day-4-passport-processing/test/passportProcessingTest.js
+++ day-4-passport-processing/test/passportProcessingTest.js
@@ -2,25 +2,31 @@ const assert = require('assert').strict
 const passportProcessing = require('../passportProcessing')
 
 describe('PassportProcessing', function () {
-  describe('validatePassport', function () {
+  describe('validate', function () {
     it('should return FALSE given a password without the birth year', function () {
-      const passportData = {ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", cid: 147, hgt: "183cm"}
-      const passportValidation = passportProcessing.validatePassport(passportData)
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", cid: 147, hgt: "183cm" }
+      const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
     })
 
     it('should return FALSE given a passport without issue year', function () {
-      const passportData = {ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, cid: 147, hgt: "183cm"}
-      const passportValidation = passportProcessing.validatePassport(passportData)
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", byr: 1937, cid: 147, hgt: "183cm" }
+      const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(!passportValidation)
-    
+    })
+
+    it('should return FLASE give a passport without birth year and issue year', function () {
+      const passportData = { ecl: "gry", pid: 860033327, eyr: 2020, hcl: "#fffffd", cid: 147, hgt: "183cm" }
+      const passportValidation = passportProcessing.validate(passportData)
+
+      assert.ok(!passportValidation)
     })
 
     xit('should return TRUE given a passport that contains the birth year,', function () {
       const passportData = { byr: 1989, pid: 118755096 }
-      const passportValidation = passportProcessing.validatePassport(passportData)
+      const passportValidation = passportProcessing.validate(passportData)
 
       assert.ok(passportValidation)
     })
