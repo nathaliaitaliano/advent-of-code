032abed61f6a496ba6f5024f18483f4eedb96f19
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-13 12:49:21 +0100

    test for validate if have data treatment for all fields passport information

--- day-4-passport-processing/test/inputParserTest.js
+++ day-4-passport-processing/test/inputParserTest.js
@@ -3,7 +3,7 @@ const inputParser = require('../inputParser')
 
 describe('InputParser', function () {
   describe('inputParser', function () {
-    it('should return 1 object given 1 input passport data', function () {
+    it('should return 1 field object given 1 field passport data', function () {
       const passportsInputData = "ecl:gry"
       const passportDataExpected = { ecl: 'gry' }
       const passportData = inputParser.parse(passportsInputData)
@@ -11,9 +11,17 @@ describe('InputParser', function () {
       assert.deepStrictEqual(passportDataExpected, passportData)
     })
 
-    it('should return 2 objects given 2 inputs passport data', function () {
-      const passportsInputData = "ecl:gry, hgt:183cm"
+    it('should return 2 fields objects given 2 fields passport data', function () {
+      const passportsInputData = "ecl:gry hgt:183cm"
       const passportDataExpected = { ecl: 'gry', hgt: '183cm' }
+      const passportData = inputParser.parse(passportsInputData)
+
+      assert.deepStrictEqual(passportDataExpected, passportData)
+    })
+
+    it('should return 8 fields objects given 8 fields passport data', function () {
+      const passportsInputData = "pid:688706448 iyr:2017 hgt:162cm cid:174 ecl:grn byr:1943 hcl:#808e9e eyr:2025"
+      const passportDataExpected = { pid: "688706448", iyr: "2017", hgt: "162cm", cid: "174", ecl:"grn", byr: "1943", hcl: "#808e9e", eyr: "2025" }
       const passportData = inputParser.parse(passportsInputData)
 
       assert.deepStrictEqual(passportDataExpected, passportData)
