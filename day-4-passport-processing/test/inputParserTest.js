9c337ac7446d65fff92a0333c5c31f6aa116f638
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-13 17:48:50 +0100

    test if the method can access the second passport fields data

--- day-4-passport-processing/test/inputParserTest.js
+++ day-4-passport-processing/test/inputParserTest.js
@@ -21,7 +21,15 @@ describe('InputParser', function () {
 
     it('should return 8 fields objects given 8 fields passport data', function () {
       const passportsInputData = "pid:688706448 iyr:2017 hgt:162cm cid:174 ecl:grn byr:1943 hcl:#808e9e eyr:2025"
-      const passportDataExpected = { pid: "688706448", iyr: "2017", hgt: "162cm", cid: "174", ecl:"grn", byr: "1943", hcl: "#808e9e", eyr: "2025" }
+      const passportDataExpected = { pid: "688706448", iyr: "2017", hgt: "162cm", cid: "174", ecl: "grn", byr: "1943", hcl: "#808e9e", eyr: "2025" }
+      const passportData = inputParser.parse(passportsInputData)
+
+      assert.deepStrictEqual(passportDataExpected, passportData)
+    })
+
+    it('should return the fields objects for the second passport data given 2 passports data', function () {
+      const passportsInputData = ["iyr:2015 cid:189 ecl:oth byr:1947 hcl:#6c4ab1 eyr:2026 \n hgt:174cm \n pid:526744288 \n \r \n pid:688706448 iyr:2017 hgt:162cm cid:174 ecl:grn byr:1943 hcl:#808e9e eyr:2025"]
+      const passportDataExpected = { "passport0": { pid: "688706448", iyr: "2017", hgt: "162cm", cid: "174", ecl: "grn", byr: "1943", hcl: "#808e9e", eyr: "2025" } }
       const passportData = inputParser.parse(passportsInputData)
 
       assert.deepStrictEqual(passportDataExpected, passportData)
