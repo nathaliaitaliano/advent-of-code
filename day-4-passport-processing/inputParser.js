a1bb66b6499a05fbb21bc2dd91e2c99e1587f0bd
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-13 12:49:48 +0100

    method refactor

--- day-4-passport-processing/inputParser.js
+++ day-4-passport-processing/inputParser.js
@@ -1,12 +1,11 @@
 const parse = (inputData) => {
   let passportsData = {}
-  const passportFields = inputData.split(", ")
+  const passportFields = inputData.split(" ")
 
   for (let i = 0; i < passportFields.length; i++) {
     const passportDataInformation = passportFields[i].split(":")
     passportsData[passportDataInformation[0]] = passportDataInformation[1]
   }
-
   return passportsData
 }
 
