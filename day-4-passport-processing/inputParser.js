89150ac84b9045bd3adfc6d8c4c4c79058e68b1e
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-14 00:40:56 +0100

    variables names refactor to better code legibility

--- day-4-passport-processing/inputParser.js
+++ day-4-passport-processing/inputParser.js
@@ -1,30 +1,29 @@
-const parse = (inputData) => {
-  const passportInput = inputData.toString().split('\n')
-  let passportsData = {}
-  const passportFields = inputData.split(" ")
-
-  for (let i = 0; i < passportFields.length; i++) {
-    const passportDataInformation = passportFields[i].split(":")
-    passportsData[passportDataInformation[0]] = passportDataInformation[1]
-  }
-  return passportsData
-}
-
-const sanitize = (inputData) => {
+const sanitize = (passportsInputData) => {
   let passportsData = []
   let fieldsPassportData = ""
 
-  for (let i = 0; i < inputData.length; i++) {
-    if (inputData[i] !== "") {
-      fieldsPassportData += ` ${inputData[i]}`
+  for (let i = 0; i < passportsInputData.length; i++) {
+    if (passportsInputData[i] !== "") {
+      fieldsPassportData += ` ${passportsInputData[i]}`
     }
-    if ((inputData[i] === "") || (i === inputData.length - 1)) {
+    if ((passportsInputData[i] === "") || (i === passportsInputData.length - 1)) {
       passportsData.push(fieldsPassportData.trim())
       fieldsPassportData = ""
     }
   }
 
   return passportsData
+}
+
+const parse = (passportsData) => {
+  const passportsFieldsData = passportsData.split(" ")
+  let passportFieldsInformation = {}
+
+  for (let i = 0; i < passportsFieldsData.length; i++) {
+    const passportDataInformation = passportsFieldsData[i].split(":")
+    passportFieldsInformation[passportDataInformation[0]] = passportDataInformation[1]
+  }
+  return passportFieldsInformation
 }
 
 module.exports = { parse, sanitize }
