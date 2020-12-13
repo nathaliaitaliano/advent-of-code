1cb7ee724c70505e24f5fff723c2986546947d1e
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-13 02:33:27 +0100

    test to validate the input type for two passports

--- day-4-passport-processing/test/inputParserTest.js
+++ day-4-passport-processing/test/inputParserTest.js
@@ -10,5 +10,13 @@ describe('InputParser', function () {
 
       assert.deepStrictEqual(passportDataExpected, passportData)
     })
+
+    it('should return 2 objects given 2 inputs passport data', function () {
+      const passportsInputData = "ecl:gry, hgt:183cm"
+      const passportDataExpected = { ecl: 'gry', hgt: '183cm' }
+      const passportData = inputParser.parse(passportsInputData)
+
+      assert.deepStrictEqual(passportDataExpected, passportData)
+    })
   })
 })
