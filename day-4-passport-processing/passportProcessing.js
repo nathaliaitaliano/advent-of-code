174c25b6eb67866936654bc9bb3878edbb6a8e47
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-14 16:35:22 +0100

    method refactor to better legibility creation of methods to validate each field parameter

--- day-4-passport-processing/passportProcessing.js
+++ day-4-passport-processing/passportProcessing.js
@@ -1,3 +1,5 @@
+const { truncate } = require("fs/promises")
+
 const validate = (passportFieldsData) => {
   const birthYear = passportFieldsData.byr
   const issueYear = passportFieldsData.iyr
@@ -8,15 +10,77 @@ const validate = (passportFieldsData) => {
   const passportId = passportFieldsData.pid
   const countryID = passportFieldsData.cid
 
+  const validateBirthYear = (birthYear) => {
+    if (birthYear && birthYear.toString().length === 4 && 1920 <= birthYear && birthYear <= 2002) {
+      return true
+    }
+    return false
+  }
+
+  const validateIssueYear = (issueYear) => {
+    if (issueYear && issueYear.toString().length === 4 && 2010 <= issueYear && issueYear <= 2020) {
+      return true
+    }
+    return false
+  }
+
+  const validateExpirationYear = (expirationYear) => {
+    if (expirationYear && expirationYear.toString().length === 4 && 2020 <= expirationYear && expirationYear <= 2030) {
+      return true
+    }
+    return false
+  }
+
+  const validateHeight = (height) => {
+    if (height) {
+      return true
+    }
+    return false
+  }
+
+  const validateHairColor = (hairColor) => {
+    if (hairColor) {
+      return true
+    }
+    return false
+  }
+
+  const validateEyeColor = (eyeColor) => {
+    if (eyeColor) {
+      const colorsPossibilities = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+      for (let i = 0; i < colorsPossibilities.length; i++) {
+        if (eyeColor === colorsPossibilities[i]) {
+          return true
+        }
+      }
+      return false
+    }
+  }
+
+  const validatePassportID = (validatePassportID) => {
+    if (passportId && passportId.toString().length === 9) {
+      return true
+    }
+    return false
+  }
+
+  const validateCountryID = (countryId) => {
+    if (countryID || !countryID) {
+      return true
+    }
+    return false
+  }
+
   return (
-    (birthYear && birthYear.toString().length === 4 && 1920 <= birthYear && birthYear <= 2002) &&
-    (issueYear && issueYear.toString().length === 4 && 2010 <= issueYear && issueYear <= 2020) &&
-    (expirationYear && expirationYear.toString().length === 4 && 2020 <= expirationYear && expirationYear <= 2030) &&
-    (height) &&
-    (hairColor) &&
-    (eyeColor && eyeColor === "amb" || eyeColor === "blu" || eyeColor === "brn" || eyeColor === "gry" || eyeColor === "grn" || eyeColor === "hzl" || eyeColor === "oth") &&
-    (passportId && passportId.toString().length === 9)) &&
-    (countryID || !countryID)
+    validateBirthYear(birthYear) &&
+    validateIssueYear(issueYear) &&
+    validateExpirationYear(expirationYear) &&
+    validateHeight(height) &&
+    validateHairColor(hairColor) &&
+    validateEyeColor(eyeColor) &&
+    validatePassportID(passportId) &&
+    validateCountryID(countryID)
+  )
 }
 
 module.exports = { validate }
