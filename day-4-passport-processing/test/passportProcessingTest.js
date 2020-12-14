24ded25c69400333c86fc3f203204e0383d8f420
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-14 12:23:37 +0100

    test to validate a passport given a eye color value oth

--- day-4-passport-processing/test/passportProcessingTest.js
+++ day-4-passport-processing/test/passportProcessingTest.js
@@ -178,6 +178,13 @@ describe('PassportProcessing', function () {
       assert.ok(!passportValidation)
     })
 
+    it('should return TRUE given a eye color value oth', function () {
+      const passportFieldsData = { ecl: "oth", pid: 860033345, eyr: 2023, hcl: "#fffffd", byr: 1990, iyr: 2017, cid: 147, hgt: "183cm" }
+      const passportValidation = passportProcessing.validate(passportFieldsData)
+
+      assert.ok(passportValidation)
+    })
+
     it('should return FALSE given a passport ID number that not contains nine digits', function () {
       const passportFieldsData = { ecl: "gry", pid: 860033, eyr: 2023, hcl: "#fffffd", byr: 1990, iyr: 2017, cid: 147, hgt: "183cm" }
       const passportValidation = passportProcessing.validate(passportFieldsData)
