848f6c10a373b1cbe22ebe6ccca4df8e96f642cf
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-14 23:39:30 +0100

    update index file

--- day-4-passport-processing/index.js
+++ day-4-passport-processing/index.js
@@ -5,12 +5,15 @@ const passportProcessing = require('./passportProcessing')
 const passportsInputData = fs.readFileSync('passport-input').toString().split('\n')
 const passportsData = inputParser.sanitize(passportsInputData)
 let validPassports = 0;
-
+console.log(passportsData)
 for (let i = 0; i < passportsData.length; i++) {
   const passportsFieldsInformation = inputParser.parse(passportsData[i])
   if (passportProcessing.validate(passportsFieldsInformation)) {
     validPassports++
+  } else {
+    console.log(passportsFieldsInformation)
   }
+  
 }
 
 console.log(validPassports)
