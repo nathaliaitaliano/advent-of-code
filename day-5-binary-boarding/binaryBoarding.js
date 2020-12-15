136205735f351f4ada776eeb470231162732210c
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-15 18:54:50 +0100

    refactor to apply KISS

--- day-5-binary-boarding/binaryBoarding.js
+++ day-5-binary-boarding/binaryBoarding.js
@@ -2,17 +2,4 @@ const findSeat = () => {
 
 }
 
-const airplaneValidate = (airplane) => {
-
-  if (airplane.length === 128) {
-    for (let i = 0; i < airplane.length; i++) {
-      if (airplane[i].length === 8) {
-        return true
-      }
-      return false
-    }
-  }
-
-}
-
-module.exports = { findSeat, airplaneValidate }
+module.exports = { findSeat }
