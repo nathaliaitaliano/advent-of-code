20642ec386cf786720e3260cfbb903fb23021628
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-15 18:18:42 +0100

    method to validate a airplane

--- day-5-binary-boarding/binaryBoarding.js
+++ day-5-binary-boarding/binaryBoarding.js
@@ -1,5 +1,18 @@
-const findSeat = (airplane) => {
-  return airplane.length
+const findSeat = () => {
+
 }
 
-module.exports = { findSeat }
+const airplaneValidate = (airplane) => {
+
+  if (airplane.length === 128) {
+    for (let i = 0; i < airplane.length; i++) {
+      if (airplane[i].length === 8) {
+        return true
+      }
+      return false
+    }
+  }
+
+}
+
+module.exports = { findSeat, airplaneValidate }
