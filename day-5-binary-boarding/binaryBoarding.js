b9ba443c544893d8d415c18e04cac14d3a66dc2f
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-16 00:04:54 +0100

    method to return the upper half for a range rows coordinate, based on the initial range for rows, given a boarding pass that start with G letter

--- day-5-binary-boarding/binaryBoarding.js
+++ day-5-binary-boarding/binaryBoarding.js
@@ -1,10 +1,3 @@
-const findSeatId = (boardingPasses) => {
-  
-  if (boardingPasses[0] === "F") {
-    return [rangeRowsInitial()[0], Math.floor((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
-  }
-}
-
 const rangeRowsInitial = () => {
   return [0, 127]
 }
@@ -13,4 +6,20 @@ const rangeColumnsInitial = () => {
   return [0, 7]
 }
 
-module.exports = { findSeatId, rangeRowsInitial, rangeColumnsInitial }
+const findSeatCoordinates = (boardingPasses) => {
+  let row = []
+  
+  if (boardingPasses[0] === "F") {
+    row = [rangeRowsInitial()[0], Math.floor((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
+  }
+  if  (boardingPasses[0] === "G") {
+    row = [rangeRowsInitial()[0], Math.ceil((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
+  }
+  return row
+}
+
+const generateSeatId = (row, colum) =>{
+
+}
+
+module.exports = { rangeRowsInitial, rangeColumnsInitial, findSeatCoordinates, generateSeatId }
