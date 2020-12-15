77b6cb10699e6cbb5a5f3fbbad8c42eac312f7ba
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-16 00:04:29 +0100

    test if a range rows coordinate is the upper half, based on the initial range for rows, given a boarding pass that start with G letter

--- day-5-binary-boarding/test/binaryBoardingTest.js
+++ day-5-binary-boarding/test/binaryBoardingTest.js
@@ -2,26 +2,33 @@ const assert = require('assert')
 const binaryBoarding = require('../binaryBoarding')
 
 describe('BinaryBoarding', function () {
-  describe('findSeatId', function () {
+  describe('generateSeatId', function () {
     it('should return a range [0, 127] initial coordinates for seats rows given a airplane', function () {
       const rangeRowsInitialExpected = [0, 127]
-
       const rangeRowsInitial = binaryBoarding.rangeRowsInitial()
+
       assert.deepStrictEqual(rangeRowsInitialExpected, rangeRowsInitial)
     })
 
     it('should return a range [0, 7] initial coordinates for seats columns given a airplane', function () {
       const rangeColumnsInitialExpected = [0, 7]
-
       const rangeColumnsInitial = binaryBoarding.rangeColumnsInitial()
+
       assert.deepStrictEqual(rangeColumnsInitialExpected, rangeColumnsInitial)
     })
 
     it('should return the lower half range rows [0, 63], based on initial range rows coordinate [0, 127], given a boarding passes that starts with the F letter', function () {
       const rangeRowsExpected = [0, 63]
       const boardingPasses = "FBFBBFFRLR"
+      const rangeRows = binaryBoarding.findSeatCoordinates(boardingPasses)
 
-      const rangeRows = binaryBoarding.findSeatId(boardingPasses)
+      assert.deepStrictEqual(rangeRowsExpected, rangeRows)
+    })
+
+    it('should return the upper half range rows [0, 64], based on initial range rows coordinate [0, 127], given a boarding passes that starts with the G letter', function () {
+      const rangeRowsExpected = [0, 64]
+      const boardingPasses = "GBFBBFFRLR"
+      const rangeRows = binaryBoarding.findSeatCoordinates(boardingPasses)
 
       assert.deepStrictEqual(rangeRowsExpected, rangeRows)
     })
