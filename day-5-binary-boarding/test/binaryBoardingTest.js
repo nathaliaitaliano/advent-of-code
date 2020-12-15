4f088597b2bb99181e36dfd755b54e348b6fa07b
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-15 23:22:46 +0100

    test if return a range with the initial coordinates for columns given a airplane

--- day-5-binary-boarding/test/binaryBoardingTest.js
+++ day-5-binary-boarding/test/binaryBoardingTest.js
@@ -2,12 +2,19 @@ const assert = require('assert')
 const binaryBoarding = require('../binaryBoarding')
 
 describe('BinaryBoarding', function () {
-  describe('findSeatID', function () {
+  describe('findSeatId', function () {
     it('should return a range [0, 127] initial coordinates for seats rows given a airplane', function () {
       const rangeRowsInitialExpected = [0, 127]
 
       const rangeRowsInitial = binaryBoarding.rangeRowsInitial()
       assert.deepStrictEqual(rangeRowsInitialExpected, rangeRowsInitial)
+    })
+
+    it('should return a range [0, 7] initial coordinates for seats columns given a airplane', function () {
+      const rangeColumnsInitialExpected = [0, 7]
+
+      const rangeColumnsInitial = binaryBoarding.rangeColumnsInitial()
+      assert.deepStrictEqual(rangeColumnsInitialExpected, rangeColumnsInitial)
     })
   })
 })
