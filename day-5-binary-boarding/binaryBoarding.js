b1520e8f53c829c97b78a6366a4637a94e6a29c3
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-16 12:12:20 +0100

    method findSeatColumnCoordinate added, for return a range column findSeatCoordinates name refactor to findSeatRowCoordinate to apply single responsability

--- day-5-binary-boarding/binaryBoarding.js
+++ day-5-binary-boarding/binaryBoarding.js
@@ -6,45 +6,50 @@ const rangeColumnsInitial = () => {
   return [0, 7]
 }
 
-const findSeatCoordinates = (boardingPass) => {
+const findSeatRowCoordinate = (boardingPass) => {
   const rowReference = boardingPass.substr(0, 7)
-  console.log(rowReference)
+  // const columnReference = boardingPass.substr(7, 9)
   let row = 0
+  let column = 0
 
   for (let i = 0; i < rowReference.length; i++) {
     if (i === 0 && rowReference[i] === "F") {
       row = [rangeRowsInitial()[0], Math.floor((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
-      console.log(row)
     }
-
     if (i != 0 && rowReference[i] === "F") {
       row = [row[0], Math.floor((row[1] - row[0]) / 2) + row[0]]
-      console.log(row)
     }
-
-    if (i ===  6 && rowReference[i] === "F") {
+    if (i === 6 && rowReference[i] === "F") {
       row = Math.min.apply(Math, row)
-      console.log(row)
     }
-
     if (i === 0 && rowReference[i] === "B") {
-      row = [rangeRowsInitial()[0], Math.ceil((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
-      console.log(row)
+      row = [Math.ceil((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2 + rangeRowsInitial()[0]), rangeRowsInitial()[1]]
     }
-    if(i != 0 && rowReference[i] === "B") {
+    if (i != 0 && rowReference[i] === "B") {
       row = [Math.ceil((row[1] - row[0]) / 2) + row[0], row[1]]
-      console.log(row)
     }
     if (i === 6 && rowReference[i] === "B") {
       row = Math.max.apply(Math, row)
-      console.log(row)
     }
   }
-    return row
+  return row
+}
+
+const findSeatColumnCoordinate = (boardingPass) => {
+  const columnReference = boardingPass
+  // const columnReference = boardingPass.substr(7, 9)
+  let column = 0
+
+  for (let i = 0; i < columnReference.length; i++) {
+    if (i === 0 && columnReference[i] === "R") {
+      column = [Math.ceil((rangeColumnsInitial()[1] - rangeColumnsInitial()[0]) / 2 + rangeColumnsInitial()[0]), rangeColumnsInitial()[1]]
+    }
+  }
+  return column
 }
 
 const generateSeatId = (row, colum) => {
 
 }
 
-module.exports = { rangeRowsInitial, rangeColumnsInitial, findSeatCoordinates, generateSeatId }
+module.exports = { rangeRowsInitial, rangeColumnsInitial, findSeatRowCoordinate, findSeatColumnCoordinate, generateSeatId }
