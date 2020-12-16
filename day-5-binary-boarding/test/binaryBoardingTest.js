18574268a670f6286070ad33ea1013618faf4b9e
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-16 01:15:18 +0100

    test if a range rows is calculated from a result reange rows of the letter before, given a letter B of a boarding pass

--- day-5-binary-boarding/test/binaryBoardingTest.js
+++ day-5-binary-boarding/test/binaryBoardingTest.js
@@ -19,15 +19,23 @@ describe('BinaryBoarding', function () {
 
     it('should return the lower half range rows [0, 63], based on initial range rows coordinate [0, 127], given a boarding passes that starts with the F letter', function () {
       const rangeRowsExpected = [0, 63]
-      const boardingPasses = "FBFBBFFRLR"
+      const boardingPasses = "F"
       const rangeRows = binaryBoarding.findSeatCoordinates(boardingPasses)
 
       assert.deepStrictEqual(rangeRowsExpected, rangeRows)
     })
 
-    it('should return the upper half range rows [0, 64], based on initial range rows coordinate [0, 127], given a boarding passes that starts with the G letter', function () {
+    it('should return the upper half range rows [0, 64], based on initial range rows coordinate [0, 127], given a boarding passes that starts with the B letter', function () {
       const rangeRowsExpected = [0, 64]
-      const boardingPasses = "GBFBBFFRLR"
+      const boardingPasses = "B"
+      const rangeRows = binaryBoarding.findSeatCoordinates(boardingPasses)
+
+      assert.deepStrictEqual(rangeRowsExpected, rangeRows)
+    })
+
+    it('should return the upper half range rows [32, 63], based on range rows coordinate [0, 63], provide for the first letter F, given a boarding passes with the second position with the B letter', function () {
+      const rangeRowsExpected = [32, 63]
+      const boardingPasses = "FB"
       const rangeRows = binaryBoarding.findSeatCoordinates(boardingPasses)
 
       assert.deepStrictEqual(rangeRowsExpected, rangeRows)
