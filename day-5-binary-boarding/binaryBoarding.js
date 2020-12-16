44a7057c18843acd8de198b41264b59605d9936b
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-16 01:32:53 +0100

    findSeatCoordinates method refactor to return a range based on a previous result given a F letter

--- day-5-binary-boarding/binaryBoarding.js
+++ day-5-binary-boarding/binaryBoarding.js
@@ -11,13 +11,21 @@ const findSeatCoordinates = (boardingPasses) => {
 
   for (let i = 0; i < boardingPasses.length; i++) {
     if (i === 0 && boardingPasses[i] === "F") {
+      
       row = [rangeRowsInitial()[0], Math.floor((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
+      console.log(row)
+    }
+    if (i != 0 && boardingPasses[i] === "F") {
+      row = [row[0], Math.floor((row[1] - row[0]) / 2) + row[0]]
+      console.log(row)
     }
     if (i === 0 && boardingPasses[i] === "B") {
       row = [rangeRowsInitial()[0], Math.ceil((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
-    } 
+      console.log(row)
+    }
     if(i != 0 && boardingPasses[i] === "B") {
       row = [Math.ceil((row[1] - row[0]) / 2), row[1]]
+      console.log(row)
     }
   }
     return row
