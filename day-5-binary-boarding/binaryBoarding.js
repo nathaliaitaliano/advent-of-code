ad1468bfb6fe5d2e7b75a3f0b503665461ba172d
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-16 02:08:41 +0100

    findSeatCoordinates method refactor to get the row reference

--- day-5-binary-boarding/binaryBoarding.js
+++ day-5-binary-boarding/binaryBoarding.js
@@ -7,35 +7,35 @@ const rangeColumnsInitial = () => {
 }
 
 const findSeatCoordinates = (boardingPass) => {
-  const rowCharacters = boardingPass.trim(3)
-  console.log(rowCharacters)
+  const rowReference = boardingPass.substr(0, 7)
+  console.log(rowReference)
   let row = 0
 
-  for (let i = 0; i < rowCharacters.length; i++) {
-    if (i === 0 && rowCharacters[i] === "F") {
+  for (let i = 0; i < rowReference.length; i++) {
+    if (i === 0 && rowReference[i] === "F") {
       row = [rangeRowsInitial()[0], Math.floor((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
       console.log(row)
     }
 
-    if (i != 0 && rowCharacters[i] === "F") {
+    if (i != 0 && rowReference[i] === "F") {
       row = [row[0], Math.floor((row[1] - row[0]) / 2) + row[0]]
       console.log(row)
     }
 
-    if (i ===  6 && rowCharacters[i] === "F") {
+    if (i ===  6 && rowReference[i] === "F") {
       row = Math.min.apply(Math, row)
       console.log(row)
     }
 
-    if (i === 0 && rowCharacters[i] === "B") {
+    if (i === 0 && rowReference[i] === "B") {
       row = [rangeRowsInitial()[0], Math.ceil((rangeRowsInitial()[1] - rangeRowsInitial()[0]) / 2)]
       console.log(row)
     }
-    if(i != 0 && rowCharacters[i] === "B") {
+    if(i != 0 && rowReference[i] === "B") {
       row = [Math.ceil((row[1] - row[0]) / 2) + row[0], row[1]]
       console.log(row)
     }
-    if (i === 6 && rowCharacters[i] === "B") {
+    if (i === 6 && rowReference[i] === "B") {
       row = Math.max.apply(Math, row)
       console.log(row)
     }
