57d3d65e3f6188475fa41c58c575ffebd0e36aae
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-17 13:01:59 +0100

    method to return the yes answers quantity given the answers of only one passengers group

--- day-6-custom-customs/customCustoms.js
+++ day-6-custom-customs/customCustoms.js
@@ -1,5 +1,13 @@
-const count = (answers) => {
+const count = (groupAnswers) => {
+  const alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "v", "x", "y", "z"]
+  let yesAnswers = 0
 
+  for (let i = 0; i < groupAnswers.length; i++) {
+    if (alphabetLetters.includes(groupAnswers[i])) {
+      yesAnswers++
+    }
+  }
+  return yesAnswers
 }
 
 module.exports = { count }
