2cbe5a549c24f55e3c7a1ed1ef86c2d4ab12d85f
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-17 15:10:28 +0100

    method to count repeatedly letters as if they were one

--- day-6-custom-customs/customCustoms.js
+++ day-6-custom-customs/customCustoms.js
@@ -1,10 +1,14 @@
 const count = (groupAnswers) => {
   const alphabetLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "v", "x", "y", "z"]
   let yesAnswers = 0
+  let questions = []
 
   for (let i = 0; i < groupAnswers.length; i++) {
     if (alphabetLetters.includes(groupAnswers[i])) {
-      yesAnswers++
+      if (!questions.includes(groupAnswers[i])) {
+        questions.push(groupAnswers[i])
+        yesAnswers++
+      }
     }
   }
   return yesAnswers
