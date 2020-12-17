b27a48ad6a36c9aa7d5264007eb6f788c06c6085
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-17 17:37:03 +0100

    test to return the passengers group answers given a data input that contains multiple answers data group

--- day-6-custom-customs/test/inputParserTest.js
+++ day-6-custom-customs/test/inputParserTest.js
@@ -5,15 +5,15 @@ describe('InputParser', function () {
   describe('parse', function () {
     it('should return a array with a empty string given a answers input data line empty', function () {
       const answersInputData = [""]
-      const answerDataExpected = [""]
+      const answerDataExpected = []
       const answerData = inputParser.sanitize(answersInputData)
 
       assert.deepStrictEqual(answerDataExpected, answerData)
     })
 
-    it('should return a string array with the answers from a passengers group, given an answers input data', function () {
-      const answersInputData = [""]
-      const answerDataExpected = [""]
+    it('should return an array with answer data from a passengers group given multiple answers groups inputs', function () {
+      const answersInputData = ["abc", "", "def"]
+      const answerDataExpected = ["abc", "def"]
       const answerData = inputParser.sanitize(answersInputData)
 
       assert.deepStrictEqual(answerDataExpected, answerData)
