e9084722fa255a8fafd5f2ce959ad4a40a7de179
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-17 18:35:30 +0100

    method refactor to return a string array data with a passenger group answers given a answer input data with multiple lines

--- day-6-custom-customs/inputParser.js
+++ day-6-custom-customs/inputParser.js
@@ -1,9 +1,14 @@
 const sanitize = (answersInputData) => {
   let answersData = []
+  let answerGroup = ""
 
   for (let i = 0; i < answersInputData.length; i++) {
     if (answersInputData[i] != "") {
-      answersData.push(answersInputData[i])
+      answerGroup += `${answersInputData[i]}`
+    }
+    if (answersInputData[i] === "" || i === answersInputData.length -1) {
+      answersData.push(answerGroup.trim())
+      answerGroup = ""
     }
   }
   return answersData
