ef0024d8a6be676b2d06648d4738f2d0ef6f9a97
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-18 11:41:44 +0100

    method to return the questions quantity that have yes answer by each group

--- day-6-custom-customs/part-two/customCustomsPart2.js
+++ day-6-custom-customs/part-two/customCustomsPart2.js
@@ -1,7 +1,10 @@
 const countQuestions = (answers) => {
+  let questionsQuantity = 0
+
   for (let i = 0; i < answers.length; i++) {
-    return answers[i].length
+    questionsQuantity += answers[i].length
   }
+  return questionsQuantity
 }
 
 module.exports = { countQuestions }
