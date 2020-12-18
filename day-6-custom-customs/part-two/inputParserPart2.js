80e1638d13e377a104a1c737a8918424743af55d
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-18 15:36:34 +0100

    sanitize method refactor to return each person answers data per passengers group

--- day-6-custom-customs/part-two/inputParserPart2.js
+++ day-6-custom-customs/part-two/inputParserPart2.js
@@ -2,17 +2,16 @@ const sanitize = (answersInputData) => {
   let answersData = []
   let answersGroup = []
 
-  for ( let i = 0; i < answersInputData.length; i++) {
-    if ( answersInputData[i] === "")
-    { 
-      answersData = []
-    }
-    if (answersInputData[i] != "")
-    {
+  for (let i = 0; i < answersInputData.length; i++) {
+    if (answersInputData[i] != "") {
       answersGroup.push(answersInputData[i])
+    }
+    if (answersInputData[i] === "" || i === answersInputData.length - 1) {
       answersData.push(answersGroup)
+      answersGroup = []
     }
   }
+  console.log(answersData)
   return answersData
 }
 
