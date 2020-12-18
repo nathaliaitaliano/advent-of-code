800adf1f4589741437fe3d35143f1b3e7c2c6fb6
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-18 11:41:19 +0100

    test if return a question quantity that have yes answer by each group

--- day-6-custom-customs/test/customCustomsTestPart2.js
+++ day-6-custom-customs/test/customCustomsTestPart2.js
@@ -3,9 +3,17 @@ const customCustoms = require('../part-two/customCustomsPart2')
 
 describe('CustomCustomsPart2', function () { 
   describe('countQuestions', function () {
-    it('should return the quantity of questions that have yes answered by all passengers of one group', function () {
+    it('should return the quantity of questions that have yes answer by all passengers of one group', function () {
       const answers = ["abc"]
       const questionsQuantityExpected = 3
+      const questionsQuantity = customCustoms.countQuestions(answers)
+
+      assert.strictEqual(questionsQuantityExpected, questionsQuantity)
+    })
+
+    it('should return the quantity of questions that have yes answer by each group', function () {
+      const answers = ["abc", "def"]
+      const questionsQuantityExpected = 6
       const questionsQuantity = customCustoms.countQuestions(answers)
 
       assert.strictEqual(questionsQuantityExpected, questionsQuantity)
