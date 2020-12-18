4fd6b217efc69a05c5955738c5dd51015f0aecdd
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-18 15:35:52 +0100

    test if return an array with answers passengers groups

--- day-6-custom-customs/test/inputParserPart2Test.js
+++ day-6-custom-customs/test/inputParserPart2Test.js
@@ -5,7 +5,7 @@ describe('InputParser', function () {
   describe('sanitize', function () {
     it('should return a array with a empty string given a answers input data line empty', function () {
       const answersInputData = [""]
-      const answerDataExpected = []
+      const answerDataExpected = [[]]
       const answerData = inputParser.sanitize(answersInputData)
 
       assert.deepStrictEqual(answerDataExpected, answerData)
@@ -14,6 +14,14 @@ describe('InputParser', function () {
     it('should return a array with the answers from a passengers group, given an answers input data', function () {
       const answersInputData = ["abcdef"]
       const answerDataExpected = [["abcdef"]]
+      const answerData = inputParser.sanitize(answersInputData)
+
+      assert.deepStrictEqual(answerDataExpected, answerData)
+    })
+
+    it('should return a array with the answers from a passengers group, given an answers for a group with multiple persons', function () {
+      const answersInputData = ["gh", "ij", "klm"]
+      const answerDataExpected = [["gh", "ij", "klm"]]
       const answerData = inputParser.sanitize(answersInputData)
 
       assert.deepStrictEqual(answerDataExpected, answerData)
