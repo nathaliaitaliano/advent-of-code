a181fd3892cd01cd66972886c86f0fcd85f4d976
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-18 12:52:30 +0100

    method to return the questions quantity that have yes answer by all passengers of a group

--- day-6-custom-customs/part-two/customCustomsPart2.js
+++ day-6-custom-customs/part-two/customCustomsPart2.js
@@ -2,8 +2,29 @@ const countQuestions = (answers) => {
   let questionsQuantity = 0
 
   for (let i = 0; i < answers.length; i++) {
-    questionsQuantity += answers[i].length
+    const answersGroup = answers[i]
+    if (answers[i].length === 1) {
+      questionsQuantity += answersGroup[i].length
+    }
+    if (answers[i] != 1) {
+      for (let k = 0; k < answersGroup.length; k++) {
+        if (answersGroup[k].length != 1) {
+          const answersPerson = answersGroup[k]
+          let questions = []
+          let questionsToCompare = answersPerson[0]
+          if (k != 0) {
+            for (let j = 0; j < answersPerson.length; j++) {
+              if (questionsToCompare.includes(answersPerson[j]) && !questions.includes(answersPerson[j])) {
+                questions.push(answersPerson[j])
+              }
+            }
+            questionsQuantity = + questions.length
+          }
+        }
+      }
+    }
   }
+  console.log(questionsQuantity)
   return questionsQuantity
 }
 
