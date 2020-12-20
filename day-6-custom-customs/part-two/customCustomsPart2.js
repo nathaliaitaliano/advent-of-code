532de30b56f5829030edf61f1d48af6d7c19fa43
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-20 16:20:26 +0100

    method to get the sum result of all results of questions that had yes answer by each person of a group

--- day-6-custom-customs/part-two/customCustomsPart2.js
+++ day-6-custom-customs/part-two/customCustomsPart2.js
@@ -1,29 +1,14 @@
-const countQuestions = (answersData) => {
-  let questionsQuantity = 0
+const countQuestionsPerGroup = (answers) => {
+  const anAnswer = answers.pop().split("")
 
-  for (let i = 0; i < answersData.length; i++) {
-    const answersGroup = answersData[i]
-    if (answersData[i].length === 1) {
-      questionsQuantity += answersGroup[0].split("").length
-    }
-    if (answersData[i] != 1) {
-      let questions = []
-      for (let k = 0; k < answersGroup.length; k++) {
-        if (answersGroup[k].length != 1) {
-          const answersPerson = answersGroup[k]
-          let questionsToCompare = answersPerson[0]
-          if (k != 0) {
-            for (let j = 0; j < answersPerson.length; j++) {
-              if (questionsToCompare.includes(answersPerson[j]) && !questions.includes(answersPerson[j])) {
-                questions.push(answersPerson[j])
-              }
-            }
-          }
-        }
-      }
-      questionsQuantity += questions.length
-    }
-  }
+  return anAnswer.filter(question => answers.every(answer => answer.includes(question))).length
+}
+
+const countQuestions = (allAnswers) => {
+  let questionsQuantity = 0
+  allAnswers.forEach(answers => {
+    questionsQuantity += countQuestionsPerGroup(answers)
+  });
   return questionsQuantity
 }
 
