a1559a5dd9b62c9f981e834599f7596627566c25
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 00:23:51 +0100

    method refactor to return the bag colors quantity

--- day-7-handy-haversacks/handyHaversacks.js
+++ day-7-handy-haversacks/handyHaversacks.js
@@ -1,9 +1,15 @@
 const countBagColors = (rules) => {
   let bagColorsQuantity = 0
+  const bagsToSearch = ["shiny gold"]
 
-  if(rules[1].includes("shiny gold")) {
-    bagColorsQuantity++
-  }
+  rules.forEach(rule => {
+    bagsToSearch.forEach(bag => {
+      if (rule[1].includes(bag)) {
+        bagsToSearch.push(rule[0])
+        bagColorsQuantity++
+      }
+    })
+  })
   return bagColorsQuantity
 }
 
