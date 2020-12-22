97ef50f17847a286dbbe4b55e6e98c2a132779dc
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-22 18:50:50 +0100

    test to return the bag colors quantity given a rule that a bag not contains a shiny gold bag

--- day-7-handy-haversacks/test/handyHaversacksTest.js
+++ day-7-handy-haversacks/test/handyHaversacksTest.js
@@ -4,7 +4,14 @@ const handyHaversacks = require('../handyHaversacks')
 describe('HandyHaversacks', function() {
   describe('countBagColors', function() {
     it('should return zero bag colors given a rule that a bag not be able to contain other bags', function () {
-      const rules = "dotted black bags contain no other bags."
+      const rules = "dotted black bags contain no other bags"
+      const bagColorsQuantityExpected = 0
+      const bagColorsQuantity = handyHaversacks.countBagColors(rules)
+
+      assert.strictEqual(bagColorsQuantityExpected, bagColorsQuantity)
+    })
+    it('should return zero bag colors given a rule that a bag not contains a shiny gold bag', function () {
+      const rules = "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags"
       const bagColorsQuantityExpected = 0
       const bagColorsQuantity = handyHaversacks.countBagColors(rules)
 
