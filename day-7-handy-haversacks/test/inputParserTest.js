98198367dabceef260fdec0c5f6e240a05ca94b6
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 14:22:58 +0100

    test for exclusion of final dots

---
+++ day-7-handy-haversacks/test/inputParserTest.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+const inputParser = require('../inputParser')
+
+describe('InputParser', function () {
+  describe('sanitize', function () {
+    it('should remove the final dots given an handy haversacks input', function () {
+      const handyHaversacksInput = "light red bags contain 1 bright white bag, 2 muted yellow bags."
+      const handyHaversacksExpected = "light red bags contain 1 bright white bag, 2 muted yellow bags"
+      const handyHaversacks = inputParser.sanitize(handyHaversacksInput)
+
+      assert.strictEqual(handyHaversacksExpected, handyHaversacks)
+    })
+  })
+})
