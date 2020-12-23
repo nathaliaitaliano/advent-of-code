83c097dd77f30c18d7ebf75d892f0017388bec01
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 15:58:28 +0100

    test if remove unnecessaries words given a input with multiple rules lines

--- day-7-handy-haversacks/test/inputParserTest.js
+++ day-7-handy-haversacks/test/inputParserTest.js
@@ -3,12 +3,20 @@ const inputParser = require('../inputParser')
 
 describe('InputParser', function () {
   describe('sanitize', function () {
-    it('should remove the final dots, the bag and bags words with one space before them, given an handy haversacks input', function () {
-      const handyHaversacksInput = "light red bags contain 1 bright white bag, 2 muted yellow bags."
-      const handyHaversacksExpected = "light red contain 1 bright white, 2 muted yellow"
+    it('should remove the bag and bags words with one space before them, given a handy haversacks input with one line', function () {
+      const handyHaversacksInput = ["light red bags contain 1 bright white bag, 2 muted yellow bags"]
+      const handyHaversacksExpected = ["light red contain 1 bright white, 2 muted yellow"]
       const handyHaversacks = inputParser.sanitize(handyHaversacksInput)
 
-      assert.strictEqual(handyHaversacksExpected, handyHaversacks)
+      assert.deepStrictEqual(handyHaversacksExpected, handyHaversacks)
+    })
+
+    it('should remove the bag and bags words with one space before them, given a handy haversacks input with multiples lines', function () {
+      const handyHaversacksInput = ["light red bags contain 1 bright white bag, 2 muted yellow bags", "dark orange bags contain 3 bright white bags, 4 muted yellow bags", "bright white bags contain 1 shiny gold bag", "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags"]
+      const handyHaversacksExpected = ["light red contain 1 bright white, 2 muted yellow", "dark orange contain 3 bright white, 4 muted yellow", "bright white contain 1 shiny gold", "muted yellow contain 2 shiny gold, 9 faded blue"]
+      const handyHaversacks = inputParser.sanitize(handyHaversacksInput)
+
+      assert.deepStrictEqual(handyHaversacksExpected, handyHaversacks)
     })
   })
 })
