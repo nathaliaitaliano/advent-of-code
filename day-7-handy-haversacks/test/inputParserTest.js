e6910123a68151d56890b9a4c38e8faf70b3f0b6
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 15:59:54 +0100

    test if a rule is converted to an array with two elements

--- day-7-handy-haversacks/test/inputParserTest.js
+++ day-7-handy-haversacks/test/inputParserTest.js
@@ -19,4 +19,14 @@ describe('InputParser', function () {
       assert.deepStrictEqual(handyHaversacksExpected, handyHaversacks)
     })
   })
+
+  describe('parse', function () {
+    it('should return an array with two elements given a one only rule data', function () {
+      const handyHaversacksInput = ["light red bags contain 1 bright white bag, 2 muted yellow bags"]
+      const handyHaversacksExpected = [["light red", "1 bright white, 2 muted yellow"]]
+      const handyHaversacks = inputParser.sanitize(handyHaversacksInput)
+
+      assert.deepStrictEqual(handyHaversacksExpected, handyHaversacks)
+    })
+  })
 })
