1a93f633a0065afc81ad608cd8e7d74f98035c69
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 14:13:48 +0100

    test to return the bag colors quantity total that contains directly or indirectly a shiny gold bag

--- day-7-handy-haversacks/test/handyHaversacksTest.js
+++ day-7-handy-haversacks/test/handyHaversacksTest.js
@@ -20,7 +20,7 @@ describe('HandyHaversacks', function() {
     })
 
     it('should return bag colors quantity given a rule that a bag contains directly a shiny gold bag', function () {
-      const rules = [["bright white]", "1 shiny gold"]]
+      const rules = [["bright white", "1 shiny gold"]]
       const bagColorsQuantityExpected = 1
       const bagColorsQuantity = handyHaversacks.countBagColors(rules)
 
@@ -28,8 +28,16 @@ describe('HandyHaversacks', function() {
     })
 
     it('should return bag colors quantity given a rule that a bag contains indirectly a shiny gold bag', function () {
-      const rules = [["bright white bags", "1 shiny gold bag"], ["dark orange bags", "3 bright white bags, 4 muted yellow bags"]]
+      const rules = [["bright white", "1 shiny gold bag"], ["dark orange", "3 bright white, 4 muted yellow"]]
       const bagColorsQuantityExpected = 2
+      const bagColorsQuantity = handyHaversacks.countBagColors(rules)
+
+      assert.strictEqual(bagColorsQuantityExpected, bagColorsQuantity)
+    })
+
+    it('should return bag colors quantity total given a rule input that a bag contains indirectly a shiny gold bag', function () {
+      const rules = [["muted yellow", "2 shiny gold, 9 faded blue"], ["bright white", "1 shiny gold"], ["dark orange", "3 bright white, 4 muted yellow"], ["light red", "1 bright white, 2 muted yellow"]]
+      const bagColorsQuantityExpected = 4
       const bagColorsQuantity = handyHaversacks.countBagColors(rules)
 
       assert.strictEqual(bagColorsQuantityExpected, bagColorsQuantity)
