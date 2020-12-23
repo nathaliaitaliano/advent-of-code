754ea898bfecad81d0c3b2f6036f91faccdde5a2
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 16:36:30 +0100

    index file created to return the result for the first part of the puzzle

---
+++ day-7-handy-haversacks/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const inputParser = require('./inputParser')
+const handyHaversacks = require('./handyHaversacks')
+
+const handyHaversacksInput = fs.readFileSync('handy-haversacks-input').toString().split('.\n')
+console.log(handyHaversacksInput)
+const handyHaversacksRules = inputParser.parse(handyHaversacksInput)
+console.log(handyHaversacksRules)
+const bagsQuantity = handyHaversacks.countBagColors(handyHaversacksRules)
+console.log(bagsQuantity)
