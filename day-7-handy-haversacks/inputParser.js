949fbd204dcfe32915e39cc9d894b95721a33cdf
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 16:24:01 +0100

    parse method to return the handy haversacks rules

--- day-7-handy-haversacks/inputParser.js
+++ day-7-handy-haversacks/inputParser.js
@@ -2,14 +2,21 @@ const sanitize = (handyHaversacksInput) => {
   let handyHaversacks = []
 
   handyHaversacksInput.forEach(handyHaversack => {
-   const handyHaversackSanitized = handyHaversack.replace(/ bag(s)?/gi, "")
+    const handyHaversackSanitized = handyHaversack.replace(/ bag(s)?/gi, "")
     handyHaversacks.push(handyHaversackSanitized)
   })
   return handyHaversacks
 }
 
+const parse = (handyHaversacksInput) => {
+  const handyHaversacks = sanitize(handyHaversacksInput)
+  let handyHaversacksRules = []
 
+  handyHaversacks.forEach(handyHaversack => {
+    const rule = handyHaversack.split(" contain ")
+    handyHaversacksRules.push(rule)
+  })
+  return handyHaversacksRules
+}
 
-module.exports = { sanitize }
-
-// "\sbag(s)?.?"
+module.exports = { sanitize, parse }
