b1e0b505623f53cb7aecb0260c62a1e513762303
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 14:14:24 +0100

    method refactor to solve the count problem

--- day-7-handy-haversacks/handyHaversacks.js
+++ day-7-handy-haversacks/handyHaversacks.js
@@ -1,16 +1,15 @@
 const countBagColors = (rules) => {
-  let bagColorsQuantity = 0
   const bagsToSearch = ["shiny gold"]
 
   rules.forEach(rule => {
     bagsToSearch.forEach(bag => {
-      if (rule[1].includes(bag)) {
-        bagsToSearch.push(rule[0])
-        bagColorsQuantity++
+      if (rule[1].includes(bag) && !bagsToSearch.includes(rule[0])) {
+          bagsToSearch.push(rule[0])
       }
+      
     })
   })
-  return bagColorsQuantity
+  return bagsToSearch.length -1
 }
 
 module.exports = { countBagColors }
