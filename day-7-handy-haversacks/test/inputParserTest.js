9e18528a1ba403496cbbe0e8aa01a47f3177d9da
nathaliaitaliano <nathalia.italiano@gmail.com>
2020-12-23 15:14:51 +0100

    test refactor

--- day-7-handy-haversacks/test/inputParserTest.js
+++ day-7-handy-haversacks/test/inputParserTest.js
@@ -3,17 +3,9 @@ const inputParser = require('../inputParser')
 
 describe('InputParser', function () {
   describe('sanitize', function () {
-    it('should remove the final dots given an handy haversacks input', function () {
+    it('should remove the final dots, the bag and bags words with one space before them, given an handy haversacks input', function () {
       const handyHaversacksInput = "light red bags contain 1 bright white bag, 2 muted yellow bags."
-      const handyHaversacksExpected = "light red bags contain 1 bright white bag, 2 muted yellow bags"
-      const handyHaversacks = inputParser.sanitize(handyHaversacksInput)
-
-      assert.strictEqual(handyHaversacksExpected, handyHaversacks)
-    })
-
-    it('should remove the bags word given an handy haversacks input', function () {
-      const handyHaversacksInput = "light red bags contain 1 bright white bag, 2 muted yellow bags."
-      const handyHaversacksExpected = "light red contain 1 bright white bag, 2 muted yellow"
+      const handyHaversacksExpected = "light red contain 1 bright white, 2 muted yellow"
       const handyHaversacks = inputParser.sanitize(handyHaversacksInput)
 
       assert.strictEqual(handyHaversacksExpected, handyHaversacks)
