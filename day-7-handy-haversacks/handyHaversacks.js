303c5a73b7f3d1106db246f4dce7b6192bee5ccc
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-04 20:16:08 +0100

    countBagsColors refactor to return a quantity of bag colors that contais directly or indirectly a shiny gold bag

--- day-7-handy-haversacks/handyHaversacks.js
+++ day-7-handy-haversacks/handyHaversacks.js
@@ -1,4 +1,7 @@
 const countBagColors = (rules, targetBag) => {
+  const bagContent = rules.values().next().value
+  console.log(bagContent)
+  if (bagContent.includes(targetBag)) return 1
   return 0
 }
 
