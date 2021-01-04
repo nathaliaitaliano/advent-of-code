803bbbf1308e1860994309c92649e46f9124980e
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-04 19:25:36 +0100

    test to count how much bags contains a shiny gold bag given a rule for a bag that not contains any other bag

--- day-7-handy-haversacks/test/handyHaversacksTest.js
+++ day-7-handy-haversacks/test/handyHaversacksTest.js
@@ -6,8 +6,10 @@ describe('HandyHaversacks', function () {
     it.only('should return zero bag colors given a rule that a bag not be able to contain other bags', function () {
       const rules = new Map()
       rules.set("dotted black", "no other")
+      const bag = "dotted black"
+      const targetBag = "no other"
       const bagColorsQuantityExpected = 0
-      const bagColorsQuantity = handyHaversacks.countBagColors(rules)
+      const bagColorsQuantity = handyHaversacks.countBagColors(bag, targetBag)
 
       assert.strictEqual(bagColorsQuantityExpected, bagColorsQuantity)
     })
