84921cb0de496e8ac045363130e59ff192f5a120
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-07 16:47:05 +0100

    data type for handyHaversacksInput changed

--- day-7-handy-haversacks/test/inputParserPartTwoTest.js
+++ day-7-handy-haversacks/test/inputParserPartTwoTest.js
@@ -4,11 +4,11 @@ const inputParser = require('../part-two/inputParserPartTwo')
 describe('InputParser', function () {
   describe('sanitize', function () {
     it('should return a rule without bag(s) words and dots given a handy haversacks rule input data', function () {
-      const handyHaversacksInput = "shiny gold bags contain 2 dark red bags."
-      const ruleExpected = "shiny gold contain 2 dark red"
+      const handyHaversacksInput = ["shiny gold bags contain 2 dark red bags."]
+      const ruleExpected = ["shiny gold contain 2 dark red"]
       const rule = inputParser.sanitize(handyHaversacksInput)
 
-      assert.strictEqual(ruleExpected, rule)
+      assert.deepStrictEqual(ruleExpected, rule)
     })
 
     it('should return more than one rule without bag(s) words and dots given a gandy haversacks rules input data with multiple lines', function () {
@@ -16,7 +16,7 @@ describe('InputParser', function () {
       const rulesExpected = ["shiny gold contain 2 dark red", "dark red contain 2 dark orange"]
       const rules = inputParser.sanitize(handyHaversacksInput)
 
-      assert.strictEqual(rulesExpected, rules)
+      assert.deepStrictEqual(rulesExpected, rules)
     })
   })
 })
