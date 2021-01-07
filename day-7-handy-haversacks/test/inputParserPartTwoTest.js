fe35e05f63d135d67a7461ed2b323d9ee9997262
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-07 16:33:50 +0100

    test to sanitize more than one rule, removing the bag(s) words and dots, given a handy haversacks input with multiple lines

--- day-7-handy-haversacks/test/inputParserPartTwoTest.js
+++ day-7-handy-haversacks/test/inputParserPartTwoTest.js
@@ -10,5 +10,13 @@ describe('InputParser', function () {
 
       assert.strictEqual(ruleExpected, rule)
     })
+
+    it('should return more than one rule without bag(s) words and dots given a gandy haversacks rules input data with multiple lines', function () {
+      const handyHaversacksInput = ["shiny gold bags contain 2 dark red bags.", "dark red bags contain 2 dark orange bags."]
+      const rulesExpected = ["shiny gold contain 2 dark red", "dark red contain 2 dark orange"]
+      const rules = inputParser.sanitize(handyHaversacksInput)
+
+      assert.strictEqual(rulesExpected, rules)
+    })
   })
 })
