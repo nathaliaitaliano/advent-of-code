cc7a6ca437388b3242d833ab5101f15572d79c4c
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-08 20:11:39 +0100

    refactor parse method refacto to aply DRY apply DRY and KISS parseBagContent created to tran parseBagContent created

--- day-7-handy-haversacks/part-two/inputParserPartTwo.js
+++ day-7-handy-haversacks/part-two/inputParserPartTwo.js
@@ -15,27 +15,20 @@ const parse = handyHaversacksInput => {
   handyHaversacks.forEach(handyHaversackRule => {
     const rule = handyHaversackRule.split(" contain ")
     const bag = rule[0]
-    let bagContent = rule[1]
-
-    if (bagContent === "no other") {
-      bagContent = [{
-        color: [],
-        quantity: 0
-      }]
-      rules.set(bag, bagContent)
-    } else {
-      const bagContents = rule[1].split(", ")
-      bagContent = []
-      bagContents.forEach(bag =>
-        bagContent.push({
-          color: bag.substring(2, bag.length),
-          quantity: Number(bag.substring(0, 1))
-        })
-      )
-      rules.set(bag, bagContent)
-    }
+    const bagContent = parseBagContent(rule[1])
+    rules.set(bag, bagContent)
   })
   return rules
 }
 
-module.exports = { sanitize, parse }
+const parseBagContent = content => {
+  if (content === "no other") {
+    return [{ color: [], quantity: 0 }]
+  }
+  return content.split(", ").map(bag => ({
+    color: bag.substring(2, bag.length),
+    quantity: Number(bag.substring(0, 1))
+  }))
+}
+
+module.exports = { parse }
