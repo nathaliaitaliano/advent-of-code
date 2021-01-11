deae3241a143e6497474422c360fa4c717b8ca56
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-11 13:14:40 +0100

    test for a bag quantity that a shiny gold bag require inside given a shiny gold bag that contains another bag

--- day-7-handy-haversacks/test/handyHaversacksPartTwoTest.js
+++ day-7-handy-haversacks/test/handyHaversacksPartTwoTest.js
@@ -3,10 +3,18 @@ const handyHaversacks = require('../part-two/handyHaversacksPartTwo')
 
 describe('HandyHaversacks', function () {
   describe('countBags', function () {
-    it('should return 0 for a shiny gold bag that does not contains another bag', function () {
-      const rule = { 'shiny gold': [{ color: [], quantity: 0 }] }
+    it('should return 0 for a quantity of bags that a shiny gold bag require inside given a rule where a shiny gold bag not contains another bag', function () {
+      const rules = { "shiny gold": [{ color: [], quantity: 0 }] }
       const bagsQuantityExpected = 0
-      const bagsQuantity = handyHaversacks.countBags(rule)
+      const bagsQuantity = handyHaversacks.countBags(rules)
+
+      assert.strictEqual(bagsQuantityExpected, bagsQuantity)
+    })
+
+    it('should return 2 for a quantity of bags that a shiny gold bag require inside given a rule where a shiny gold bag contains just one bag', function () {
+      const rules = { "shiny gold": [{ color: "dark red", quantity: 1 }], "dark red": [{ color: "dark orange", quantity: 1 }], "dark orange": [{ color: [], quantity: 0 }] }
+      const bagsQuantityExpected = 2
+      const bagsQuantity = handyHaversacks.countBags(rules)
 
       assert.strictEqual(bagsQuantityExpected, bagsQuantity)
     })
