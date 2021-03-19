dc81253f4fa2ae04e4502faddbbe490f1f26455c
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-19 12:51:49 +0100

    test folder added

---
+++ day-8-handheld-halting/test/handheldHaltingTest.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+const handheldHalting = require('../handheldHalting')
+
+describe('HandheldHalting', function () {
+  describe('', function () {
+    it('', function () {
+    })
+  })
+})
