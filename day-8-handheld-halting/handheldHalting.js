90ddd45ca85b3575217a43a64c17231be6f651fc
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-24 21:42:28 +0100

    should sum to accumulator given an instruction with acc operation

--- day-8-handheld-halting/handheldHalting.js
+++ day-8-handheld-halting/handheldHalting.js
@@ -1,7 +1,11 @@
 const calculateAccumulatorValue = (instructions) => {
   let accumulator = 0;
 
-  return accumulator;
+
+  if (instructions[0].operation = "acc") {
+    accumulator += instructions[0].argument;
+    return accumulator;
+  }
 }
 
 module.exports = { calculateAccumulatorValue }
--- day-8-handheld-halting/test/handheldHaltingTest.js
+++ day-8-handheld-halting/test/handheldHaltingTest.js
@@ -3,9 +3,17 @@ const handheldHalting = require('../handheldHalting')
 
 describe('HandheldHalting', function () {
   describe('calculateAccumulatorValue', function () {
-    it('should return value 0 for the accumulator(ACC) before starts run the instructions', function () {
-      const instructions = { acc: ["+", 2], jmp: ["-", 3], nop: ["+", 0] };
+    xit('should return value 0 for the accumulator(ACC) before starts run the instructions', function () {
+      const instructions = [{ operation: "acc", argument: 2 }];
       const accumulatorExpected = 0;
+      const accumulator = handheldHalting.calculateAccumulatorValue(instructions);
+
+      assert.strictEqual(accumulator, accumulatorExpected);
+    })
+
+    it('should sum +2 on accumulator value given an instruction with operation: "acc" and argument: 2', function () {
+      const instructions = [{ operation: "acc", argument: 2 }];
+      const accumulatorExpected = 2;
       const accumulator = handheldHalting.calculateAccumulatorValue(instructions);
 
       assert.strictEqual(accumulator, accumulatorExpected);
