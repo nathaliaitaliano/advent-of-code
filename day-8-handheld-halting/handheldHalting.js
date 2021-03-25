f5dd19f05e8c893493f06d6bebe8aa2db61b8aac
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-25 11:23:13 +0100

    should jump for the next instructions givens an operation jmp with a positive value for argument

--- day-8-handheld-halting/handheldHalting.js
+++ day-8-handheld-halting/handheldHalting.js
@@ -1,11 +1,21 @@
 const calculateAccumulatorValue = (instructions) => {
   let accumulator = 0;
+  let index = 0;
+  const indexOfInstructionsExecuted = [];
 
-  for (let i = 0; i < instructions.length; i++) {
-    const instruction = instructions[i];
-    if (instruction.operation === "acc") accumulator += instruction.argument;
-    if (instruction.operation === "jmp") i += instruction.argument - 1;
-  }
+  do {
+    const instruction = instructions[index];
+    indexOfInstructionsExecuted.push(index);
+
+    if (instruction.operation === "acc") {
+      accumulator += instruction.argument;
+      index += 1;
+    } else if (instruction.operation === "jmp") {
+      index += instruction.argument;
+    } else {
+      index += 1;
+    }
+  } while (!indexOfInstructionsExecuted.includes(index) && index < instructions.length);
   return accumulator;
 }
 
