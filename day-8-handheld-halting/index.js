63a2e419d3629f17838092c5875a4cbf84540fa8
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-25 15:01:17 +0100

    treatment of instructions input data

--- day-8-handheld-halting/index.js
+++ day-8-handheld-halting/index.js
@@ -3,3 +3,4 @@ const handheldHalting = require('./handheldHalting');
 const inputParser = require('./inputParser');
 
 const instructionsInput = fs.readFileSync('boot-instructions-input').toString().split('\n');
+const instructions = inputParser.parse(instructionsInput);
