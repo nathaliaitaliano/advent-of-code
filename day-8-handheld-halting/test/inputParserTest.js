9a9db3d464fe698fec36b294d990537addae7343
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-26 16:34:41 +0100

    mend

--- day-8-handheld-halting/test/inputParserTest.js
+++ day-8-handheld-halting/test/inputParserTest.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const inputParser = require('../part-one/inputParser');
+const inputParser = require('../inputParser');
 
 describe('InputParser', function () {
   describe('sanitize', function () {
