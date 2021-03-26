8233e4031ce3238f4b1d5ffdd5e74e9320ee8f54
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-26 16:32:39 +0100

    organizing code in folders for better split the first and second part of puzzle

--- day-8-handheld-halting/part-one/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const inputParser = require('./inputParser');
-const handheldHalting = require('./handheldHalting');
-
-const instructionsInput = fs.readFileSync('boot-instructions-input').toString().split('\n');
-const instructions = inputParser.parse(instructionsInput);
-const accumulator = handheldHalting.calculateAccumulatorValue(instructions);
-console.log(accumulator);
