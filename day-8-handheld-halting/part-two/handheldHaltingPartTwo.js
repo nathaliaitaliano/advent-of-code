bef72e8e1595bb65758d45048ff954029f96184a
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-03-30 18:45:06 +0200

    refactor to remove unnecessary code

--- day-8-handheld-halting/part-two/handheldHaltingPartTwo.js
+++ day-8-handheld-halting/part-two/handheldHaltingPartTwo.js
@@ -1,43 +1,46 @@
 const calculateAccumulatorValue = (instructions) => {
   let accumulator = 0;
   let index = 0;
-  const indexOfInstructionsExecuted = [];
 
-  do {
-    const instruction = instructions[index];
+  if (isAnInfiniteLoop(instructions)) {
+    return fixInfiniteLoop(instructions);
+  } else {
+    do {
+      const instruction = instructions[index];
 
-    if (instruction.operation === "acc") {
-      accumulator += instruction.argument;
-      index += 1;
-    } else if (instruction.operation === "jmp") {
-      index += instruction.argument;
-    } else {
-      index += 1;
-    }
-  } while (index < instructions.length);
-  return accumulator;
+      if (instruction.operation === "acc") {
+        accumulator += instruction.argument;
+        index += 1;
+      } else if (instruction.operation === "jmp") {
+        index += instruction.argument;
+      } else {
+        index += 1;
+      }
+    } while (index < instructions.length);
+    return accumulator;
+  }
 }
 
 const isAnInfiniteLoop = (instructions) => {
   let index = 0;
   const indexOfInstructionsExecuted = [];
-  const infiniteLoop = true;
 
   do {
     const instruction = instructions[index];
+    const isAnInfiniteLoop = indexOfInstructionsExecuted.includes(index);
 
-    if (indexOfInstructionsExecuted.includes(index)) {
-      return infiniteLoop;
-    } else {
+    if (!isAnInfiniteLoop) {
       indexOfInstructionsExecuted.push(index);
       if (instruction.operation === "jmp") {
         index += instruction.argument;
       } else {
         index += 1;
       }
+    } else {
+      return isAnInfiniteLoop;
     }
   } while (index < instructions.length);
-  calculateAccumulatorValue(instructions);
+  return !isAnInfiniteLoop;
 }
 
 module.exports = { calculateAccumulatorValue, isAnInfiniteLoop }
