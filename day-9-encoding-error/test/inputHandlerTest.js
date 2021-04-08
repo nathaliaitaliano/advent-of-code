284084d1f8bd4a45845061a647130db89716be39
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-04-08 23:28:52 +0200

    parse data of an encoding error input to integer

--- day-9-encoding-error/test/inputHandlerTest.js
+++ day-9-encoding-error/test/inputHandlerTest.js
@@ -2,19 +2,29 @@ const assert = require('assert');
 const inputHandler = require('../inputHandler');
 
 describe('InputHandler', function () {
-  describe('InputHandler', function () {
-    describe('split', function () {
-      it('Should return an object with two lists: one for preamble numbers and other for the rest of numbers given an input', function () {
-        const input = ["1", "2", "3"];
-        const preambleNumbersQuantity = 2;
-        const encodingErrorSplitedExpected = {
-          preambleNumbers: [1, 2],
-          otherNumbers: [3]
-        };
-        const encodingErrorSplited = inputHandler.split(input, preambleNumbersQuantity);
+  describe('parser', function () {
+    it('Should return a list of elements type number given an input', function () {
+      const input = ["1", "2", "3"];
+      const encodingErrorExpected = [1, 2, 3];
+      const encodingError = inputHandler.parser(input);
 
-        assert.deepStrictEqual(encodingErrorSplited, encodingErrorSplitedExpected);
-      })
+      assert.deepStrictEqual(encodingError, encodingErrorExpected);
+    })
+  })
+})
+
+describe('InputHandler', function () {
+  describe('split', function () {
+    it('Should return an object with two lists: one for preamble numbers and other for the rest of numbers given an input', function () {
+      const input = ["1", "2", "3"];
+      const preambleNumbersQuantity = 2;
+      const encodingErrorSplitedExpected = {
+        preambleNumbers: [1, 2],
+        otherNumbers: [3]
+      };
+      const encodingErrorSplited = inputHandler.split(input, preambleNumbersQuantity);
+
+      assert.deepStrictEqual(encodingErrorSplited, encodingErrorSplitedExpected);
     })
   })
 })
