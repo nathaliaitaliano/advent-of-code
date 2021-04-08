5501707818bb128aa73ac2200af0d7a3f930ce7a
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-04-08 14:39:01 +0200

    boilerplate to start testing

---
+++ day-9-encoding-error/test/encodingErrorTest.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const encodingError = require('../encodingError');
+
+describe('', function () {
+  describe('', function () {
+    it('', function () {
+
+    })
+  })
+})
