497b729bc8d205a5db6c5978cd8d6122d2179748
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-04-08 18:43:39 +0200

    renaming test to better understanding

--- day-9-encoding-error/test/encodingErrorTest.js
+++ day-9-encoding-error/test/encodingErrorTest.js
@@ -3,12 +3,23 @@ const encodingError = require('../encodingError');
 
 describe('EncodingError', function () {
   describe('invalidateNumber', function () {
-    it.only('Should return a number from othersNumbers that is not a sum of two numbers from preambleNumbers', function () {
+    it('Should return a number from othersNumbers that is not a sum of two numbers from preambleNumbers given a preambleNumbers with two numbers', function () {
       const encodingErrorInput = {
         preambleNumbers: [1, 2],
         otherNumbers: [4]
       };
       const invalidNumberExpected = 4;
+      const invalidNumer = encodingError.invalidateNumber(encodingErrorInput);
+
+      assert.strictEqual(invalidNumer, invalidNumberExpected);
+    })
+
+    it.only('Should return a number from othersNumbers that is not a sum of any two numbers from preambleNumbers given a preambleNumbers with three numbers', function () {
+      const encodingErrorInput = {
+        preambleNumbers: [1, 2, 3],
+        otherNumbers: [4, 6]
+      };
+      const invalidNumberExpected = 6;
       const invalidNumer = encodingError.invalidateNumber(encodingErrorInput);
 
       assert.strictEqual(invalidNumer, invalidNumberExpected);
