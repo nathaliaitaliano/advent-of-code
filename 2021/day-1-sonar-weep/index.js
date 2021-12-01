5352e9985256b9a41d8f4d88895a483f1d0bdbda
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-01 20:54:24 +0100

    to get puzzle result

--- 2021/day-1-sonar-weep/index.js
+++ 2021/day-1-sonar-weep/index.js
@@ -1,4 +1,8 @@
-const fs = require('fs');
-const reportRepair = require('./sonarWeep');
+const fs = require('fs')
+const sonarWeep = require('./sonarWeep')
 
-const inputContent = fs.readFileSync('entries-input').toString().split('\n');
+const entries = fs.readFileSync('entries-input').toString().split('\n').map((measurement) => parseInt(measurement, 10))
+
+var measurements = sonarWeep.findMeasurements(entries)
+
+console.log(measurements)
