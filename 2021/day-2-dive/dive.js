df32e9cbb28669c3c90e4a9bae3bd9a9cfa4c1ab
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-03 18:09:13 +0100

    to get positions given instructions

--- 2021/day-2-dive/dive.js
+++ 2021/day-2-dive/dive.js
@@ -1,5 +1,23 @@
 const calculatePositions = (entries) => {
-  return { horizontal: 0, depth: 0 }
+var instructions = getInstructions(entries)
+
+const positions = {
+  horizontal: 0,
+  depth: 0
 }
+
+  instructions.forEach(instruction => {
+    if(instruction.command == "forward") positions.horizontal += instruction.value
+    if(instruction.command == "down") positions.depth += instruction.value
+    if(instruction.command == "up") positions.depth -= instruction.value
+  })
+
+  return positions
+}
+
+const getInstructions = (entries) => entries.map(entrie => {
+    const values = entrie.split(" ");
+    return {command: values[0], value: parseInt(values[1])}
+})
 
 module.exports = { calculatePositions }
