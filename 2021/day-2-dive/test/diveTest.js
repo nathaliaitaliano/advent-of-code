f3c8485bee3b40eea668557b3c69d8b7fe92fa68
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-03 18:08:38 +0100

    advent of code test case

--- 2021/day-2-dive/test/diveTest.js
+++ 2021/day-2-dive/test/diveTest.js
@@ -2,14 +2,41 @@ const assert = require('assert')
 const dive = require('../dive')
 
 describe('Dive', function () {
-    describe('multiplyPositions', function () {
-        it('should return 0 for positions given an entries without instructions', function () {
-            const entries = []
-            const positionsExpected = { horizontal: 0, depth: 0 }
+    describe('calculatePositions', function () {
+      it('should increases horizontal position given an instruction foward', function () {
+          const entries = ["forward 1"]
+          const positionsExpected = { horizontal: 1, depth: 0 }
 
-            const result = dive.calculatePositions(entries)
+          const result = dive.calculatePositions(entries)
 
-            assert.deepEqual(result, positionsExpected)
-        })
+          assert.deepEqual(result, positionsExpected)
+      })
+
+      it('should increases depth position given an instruction down', function () {
+        const entries = ["forward 1", "down 1"]
+        const positionsExpected = { horizontal: 1, depth: 1 }
+
+        const result = dive.calculatePositions(entries)
+
+        assert.deepEqual(result, positionsExpected)
+      })
+
+      it('should decreases depth position given an instruction up', function () {
+        const entries = ["forward 1", "down 1", "up 2"]
+        const positionsExpected = { horizontal: 1, depth: -1 }
+
+        const result = dive.calculatePositions(entries)
+
+        assert.deepEqual(result, positionsExpected)
+      })
+
+      it('should return position given an entries with instructions', function () {
+        const entries = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+        const positionsExpected = { horizontal: 15, depth: 10 }
+
+        const result = dive.calculatePositions(entries)
+
+        assert.deepEqual(result, positionsExpected)
+      })
     })
 })
