04b0856258890d7a9c46d5638f90575d8c4c14d7
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-03 18:07:31 +0100

    to get position multiplication result

--- 2021/day-2-dive/index.js
+++ 2021/day-2-dive/index.js
@@ -1,3 +1,9 @@
 const dive = require('./dive')
 const inputParser = require ('./inputParser')
 const entries = inputParser.parse('entries-input')
+
+const instructions = dive.calculatePositions(entries)
+
+console.log(entries)
+console.log(instructions)
+console.log(instructions.horizontal * instructions.depth)
