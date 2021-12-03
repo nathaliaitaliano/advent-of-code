d100e0be805bf02bc09f3f9f278f28e3298c2e17
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-04 00:16:31 +0100

    code refactor to use reduce

--- 2021/day-2-dive/divePartTwo.js
+++ 2021/day-2-dive/divePartTwo.js
@@ -1,34 +1,28 @@
-const calculatePositions = (entries) => {
-var instructions = getInstructions(entries)
+const calculatePositions = entries => getInstructions(entries).reduce((positions, instruction) => {
+  switch (instruction.command) {
+    case "forward":
+      positions.horizontal += instruction.value
+      positions.depth += positions.aim * instruction.value
+      break
 
-const positions = {
+    case "down":
+      positions.aim += instruction.value
+      break
+
+    case "up":
+      positions.aim -= instruction.value
+      break
+  }
+  return positions
+}, {
   horizontal: 0,
   depth: 0,
-  aim : 0
-}
-
-  instructions.forEach(instruction => {
-    switch (instruction.command) {
-      case "forward":
-        positions.horizontal += instruction.value
-        positions.depth += positions.aim * instruction.value
-        break
-
-      case "down":
-        positions.aim += instruction.value
-        break
-
-      case "up":
-        positions.aim -= instruction.value
-        break
-    }
-  })
-  return positions
-}
+  aim: 0
+})
 
 const getInstructions = (entries) => entries.map(entrie => {
-    const values = entrie.split(" ");
-    return {command: values[0], value: parseInt(values[1])}
+  const values = entrie.split(" ");
+  return { command: values[0], value: parseInt(values[1]) }
 })
 
 module.exports = { calculatePositions }
