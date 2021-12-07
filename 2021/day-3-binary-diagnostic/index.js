c5eac772d1772d08c15b3224d38276c88d2d56d3
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-07 12:38:09 +0100

    to get day 3 puzzle result

--- 2021/day-3-binary-diagnostic/index.js
+++ 2021/day-3-binary-diagnostic/index.js
@@ -1,3 +1,7 @@
 const binaryDiagnostic = require('./binaryDiagnostic')
-const inputParser = require ('./inputParser')
-const entries = inputParser.parse('entries-input')
+const parser = require ('./parser')
+const entries = parser.parse('entries-input')
+
+const diagnostic = binaryDiagnostic.generateDiagnostic(entries)
+
+console.log(parser.parseToDecimal(diagnostic.gammaRate) * parser.parseToDecimal(diagnostic.epilsonRate))
