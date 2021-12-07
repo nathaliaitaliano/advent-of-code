f14544f9a17f1ca81f8eea55ec791afb10d4e1df
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-12-07 12:03:54 +0100

    to generate diagnotic

--- 2021/day-3-binary-diagnostic/binaryDiagnostic.js
+++ 2021/day-3-binary-diagnostic/binaryDiagnostic.js
@@ -1,4 +1,28 @@
-const generateDiagnostic = (entries) => {
+const generateDiagnostic = entries => {
+  const diagnostic = {
+    gammaRate: "",
+    epilsonRate: ""
+  }
+
+  const numbersByColumnGrouped = traverse(entries)
+
+  numbersByColumnGrouped.forEach(number => {
+    const zeros = number.filter(bit => bit === "0").length
+    if(zeros > number.length/2) {
+      diagnostic.gammaRate += "0"
+      diagnostic.epilsonRate += "1"
+    } else {
+      diagnostic.gammaRate += "1"
+      diagnostic.epilsonRate += "0"
+    }
+  })
+  console.log(diagnostic)
+  return diagnostic
 }
+
+const traverse = entries => entries.reduce((result, report) => {
+  report.split("").forEach((bit, index) => result[index].push(bit))
+     return result
+  }, Array.from({length: entries[0].length}, () => []))
 
 module.exports = { generateDiagnostic }
--- 2021/day-3-binary-diagnostic/test/binaryDiagnosticTest.js
+++ 2021/day-3-binary-diagnostic/test/binaryDiagnosticTest.js
@@ -2,8 +2,21 @@ const assert = require('assert')
 const binaryDiagnostic = require('../binaryDiagnostic')
 
 describe('BinaryDiagnostic', function () {
-    describe('', function () {
-      it('', function () {
+    describe('generateDiagnostic', function () {
+      it('should return the bit mostly common as gammaRate value and the last common bit as epilsonRate value given a report', function () {
+        const report = ["0", "1", "1"]
+        const diagnosticExpected = { gammaRate: "1", epilsonRate: "0"}
+        const result = binaryDiagnostic.generateDiagnostic(report)
+
+        assert.deepEqual(result, diagnosticExpected)
+      })
+
+      it('should return, for each column of a binary number, the bit mostly common as gammaRate value and the last common bit as epilsonRate value given a report', function () {
+        const report = ["00", "11", "10"]
+        const diagnosticExpected = { gammaRate: "10", epilsonRate: "01"}
+        const result = binaryDiagnostic.generateDiagnostic(report)
+
+        assert.deepEqual(result, diagnosticExpected)
       })
     })
 })
